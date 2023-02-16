a463f439467a9cce57ec9620980d4fd96468fa48
Kanyire Kwadwo Wilfred <wilfredkanyire50@gmail.com>
2023-02-16 10:37:29 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 1 (Success) 
+ * main - Prints "and that piece of art is useful" - - Dora Korpar
+ * followed by a new line, to standard error.
+ * Return: Always 1 
  */
 int main(void)
 {
