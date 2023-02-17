0582587d64c1f1f36ad3b766c2c199be56587b67
Kanyire Kwadwo Wilfred <wilfredkanyire50@gmail.com>
2023-02-17 15:05:07 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char n;
+
+	n = 97;
+	while (n < 123)
+	{
+		if (n != 101 && n != 113)
+		{
+			putchar(n);
+			n++;
+		}else
+		{
+			n++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
