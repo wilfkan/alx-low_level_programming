f567a063a8a7dfd9396c5211fd532dfbc2504ca8
Kanyire Kwadwo Wilfred <wilfredkanyire50@gmail.com>
2023-02-17 15:28:12 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int w;
+
+	for (w = 0; w < 10; w++)
+	{
+		printf("%w", w);
+	}
+	printf("\n");
+	return (0);
+}
