0983e8cfc49ee797739dbf436ef283113ef6b742
Kanyire Kwadwo Wilfred <wilfredkanyire50@gmail.com>
2023-02-17 13:38:16 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, 1);
 	}
-	else if (1 == )
+	else if (1 == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, 1);
 	}
