e75d765e782eed0c3ae1c36e1236abf0be0ffa16
Kanyire Kwadwo Wilfred <wilfredkanyire50@gmail.com>
2023-02-17 14:07:15 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char c = 'a';
+    while (c <= 'z')
+    {
+	    printf("%c ", c);
+	    c++;
+    }
+    return (0);
+}
