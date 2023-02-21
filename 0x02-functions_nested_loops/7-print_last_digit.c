303649db9b60e43de08b25576add67e1cc060cf1
Kanyire Kwadwo Wilfred <wilfredkanyire50@gmail.com>
2023-02-21 10:38:05 -0800

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_last_digit -  prints the last digit of a number
+ *
+ * @x: number
+ * Return: value of last digit
+ */
+int print_last_digit(int x)
+{
+	if (x < 0)
+		n *= -1;
+
+	_putchar('0' + (n % 10));
+
+	return (n % 10);
+}
