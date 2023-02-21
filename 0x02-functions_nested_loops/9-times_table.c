7feae30add1474903ea18b4438814173794dd3c1
Kanyire Kwadwo Wilfred <wilfredkanyire50@gmail.com>
2023-02-21 11:05:00 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,9 +6,7 @@
  */
 void times_table(void)
 {
-	int num;
-	int mult;
-	int prod;
+	int num, mult, prod;
 
 	for (num = 0; num <= 9; num++)
 	{
@@ -19,12 +17,13 @@ void times_table(void)
 			_putchar(',');
 			_putchar(' ');
 
-			prod = num = mult;
+			prod = num * mult;
 
 			if (prod <= 9)
 				_putchar(' ');
 			else
 				_putchar((prod / 10) + '0');
+			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
 	}
