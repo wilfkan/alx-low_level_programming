5dd97e6bf8e4f083b1a43699bad84574793157e8
Kanyire Kwadwo Wilfred <wilfredkanyire50@gmail.com>
2023-02-21 09:17:27 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,7 +3,6 @@
 /**
  * main - prints alphabets in lower case
  *
- * Return: Always 0
  */
 void print_alphabet(void)
 {
@@ -17,6 +16,4 @@ void print_alphabet(void)
 		letter++;
 	}
 	_putchar('\n');
-
-	return (0);
 }
