9dcf4991e3adb3591191748ec9e3e969717630b8
Kanyire Kwadwo Wilfred <wilfredkanyire50@gmail.com>
2023-02-23 02:56:31 -0800

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *more_numbers - prints the numbers from 0 to 14 ten times
+ *
+ */
+void more_numbers(void)
+{
+	char lett;
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		for (char = 0; char < 15; char++)
+		{
+			if (char >= 10)
+				_putchar((char / 10) + 48);
+			_putchar((char % 10) + 48);
+		}
+		_putchar('\n')
+	}
+}
