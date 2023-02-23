5cf6efee9531d35d03fb664b15850e6a45b3b120
Kanyire Kwadwo Wilfred <wilfredkanyire50@gmail.com>
2023-02-23 02:39:36 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_numbers - prints numbers from 0 to 9
+ *
+ *
+ */
+void print_numbers(void)
+{
+	int ch;
+
+	for (ch = 48; ch < 58; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
