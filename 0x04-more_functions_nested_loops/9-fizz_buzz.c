490c2b4b332212cd5dbaeff4cf1ea6ca63e1c141
Kanyire Kwadwo Wilfred <wilfredkanyire50@gmail.com>
2023-02-23 03:24:52 -0800

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *main - prints numbers from 1 to 100
+ *Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	n = 1;
+	printf("%d", n);
+	for (n = 2; n <= 100; n++)
+	{
+		if ((n % 3 == 0) && (n % 5 == 0))
+		{
+			printf("Fizzbuzz");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fiz");
+		}
+		else
+		{
+			printf("%d", n);
+		}
+	}
+	printf("\n");
+	return (0);
+}
