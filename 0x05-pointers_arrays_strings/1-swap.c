8f7e51807325e0b6e095a1bb7845b9fdb97e539a
Kanyire Kwadwo Wilfred <wilfredkanyire50@gmail.com>
2023-02-27 10:28:19 -0800

    git

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#inlcude "main.h"
+/**
+ *swap_int - swaps the values of two integers
+ *
+ *@a: integer 1
+ *@b: integer 2
+ *
+ *Return: no return
+ */
+void swap_int(int *a, int *b)
+{
+	int subete = *a;
+	*a = *b;
+	*b = subete;
+}
