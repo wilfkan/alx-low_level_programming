ccc2c07ff52bb5011639c932865dca41e2cf8edd
Kanyire Kwadwo Wilfred <wilfredkanyire50@gmail.com>
2023-02-27 12:44:05 -0800

    git

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,22 +1,20 @@
-#include "main.h"
+
+#include"main.h"
 
 /**
- * _puts - prints a string, followed by a new line
- * @str: input string
- * Return: no return
- */
+ * _puts - prints a string, followed by a newline to stdout
+ *
+ * @str: string parameter to print
+ *
+ * Return: Nothing
+*/
+
 void _puts(char *str)
 {
-	int count = 0;
-
-	while (count >= 0)
+	while (*str != '\0')
 	{
-		if (str[count] == '\0')
-		{
-			_putchar('\n');
-			break;
-		}
-		_putchar(str[count]);
-		count++;
+		_putchar(*str + 0);
+		++str;
 	}
+	_putchar('\n');
 }
