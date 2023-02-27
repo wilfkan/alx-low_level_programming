440c96d617f8fc56bab50e9b5033f2caf66dffaf
Kanyire Kwadwo Wilfred <wilfredkanyire50@gmail.com>
2023-02-27 14:19:39 -0800

    git

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_array - prints n elements of an array of integers
+ *
+ * @n: number of elements in array
+ * Return: no return
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (i = 0; i < n; ++1)
+	{
+		if (i != (n - 1))
+			printf("%d, ", a[i]);
+		else
+			printf("%d, ", a[i]);
+	}
+	printf('\n')
+}
