25cd1fa0c5b78b9f813be99c215dbda7276619cd
Kanyire Kwadwo Wilfred <wilfredkanyire50@gmail.com>
2023-02-27 14:04:21 -0800

    git

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ *
+ * @str: string
+ *
+ * Return: no return
+ */
+void puts_half(char *str)
+{
+	int count = 0, l;
+
+	while (count >= 0)
+	{
+		if (str[count] =='\0')
+			break;
+		count++;
+	}
+
+	if (count % 2 == 1)
+		l = count / 2;
+	else
+		l = (count - 1) / 2;
+	for (l++; l < count; l++)
+		_putchar(str[l]);
+	_putchar('\n')
