6c580ca9ad8d6c6442ccffe74912927b3881333d
Kanyire Kwadwo Wilfred <wilfredkanyire50@gmail.com>
2023-03-01 10:43:55 -0800

    6

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,8 +3,10 @@
  * _strncpy - copies a string
  * @dest: destination
  * @src: source
- * Return: no return
+ * @n: amount of bytes from src
+ * Return: the pointer to dest
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
