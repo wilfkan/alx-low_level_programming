f8cb841e68a7f8b48891304e0a051efff06e41da
Kanyire Kwadwo Wilfred <wilfredkanyire50@gmail.com>
2023-03-06 08:49:32 -0800

    l

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,12 +11,12 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, sum_1 = 0, sum_2 =0;
+	int i, sum1 = 0, sum2 =0;
 
 	for(i = 0; i < size; i++)
 	{
-		sum_1 += *(a + (size * i + i));
-		sum_2 += *(a + (size * i + size - 1 - i));
+		sum1 += *(a + (size * i + i));
+		sum2 += *(a + (size * i + size - 1 - i));
 	}
 	printf("%d, ", sum_1);
 	printf("%d\n", sum_2);
