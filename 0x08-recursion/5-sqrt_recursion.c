930457e5da75e5643deb00e3514f4799605e1081
Kanyire Kwadwo Wilfred <wilfredkanyire50@gmail.com>
2023-03-09 03:33:59 -0800

    5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * sqrt_check - checks for the square root of c
+ *
+ * @g: sqrt
+ * @c: number to sqrt
+ *
+ * Return: -1 or sqrt of c
+ */
+int sqrt_check(int g, int c)
+{
+	if (g * g ==c)
+		return (g);
+	if (g * g > c)
+		return (-1);
+	return (sqrt_check(g + 1, c)
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ *
+ * @n: number to sqrt
+ *
+ * Return: if n does not have a natural square root, the function should return -1
+ */
+int _sqrt_recursion(int n)
+{
+        if (n == 0)
+	        return (0);
+
+	 return (sqrt_check(1, n));
+}
