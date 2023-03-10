24504a5baa64180ec3505423b28607b80a557213
Kanyire Kwadwo Wilfred <wilfredkanyire50@gmail.com>
2023-03-10 14:55:25 -0800

    l

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - Entry point
+* @argc: the number of argument
+* @argv: a pointer to argument strings
+* Return: always 1
+*/
+
+int main(int argc, char *argv[])
+{
+
+	if (argc >= 0)
+		puts(argv[0]);
+	return (0);
+}
