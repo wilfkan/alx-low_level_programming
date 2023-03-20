be5dffc31ecc8e33546b6e02bab11e57289665e2
Kanyire Kwadwo Wilfred <wilfredkanyire50@gmail.com>
2023-03-20 15:25:57 -0700

    done

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints the file input into the preprocessor
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
