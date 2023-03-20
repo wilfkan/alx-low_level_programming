6df2dc4b9c1404a6a96829d2973a1157d41f0868
Kanyire Kwadwo Wilfred <wilfredkanyire50@gmail.com>
2023-03-20 14:57:03 -0700

    done

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * free_dog - check the code
+ * @d: decimal
+ * Return: Always 0
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		if (d->name)
+			free(d->name);
+		if (d->owner)
+			free(d->owner);
+		free(d);
+	}
+}
