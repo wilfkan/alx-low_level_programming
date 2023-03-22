a2cdd91d9038a541d242bac62d980b0083c7a634
Kanyire Kwadwo Wilfred <wilfredkanyire50@gmail.com>
2023-03-22 12:11:16 -0700

    resolved

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -3,8 +3,8 @@
 /**
  * op_add - adds two numbers
  *
- * @a: first number
- * @b: second number
+ * @a: first number to be added
+ * @b: second number to be added
  *
  * Return: the result of the operation
  */
@@ -16,8 +16,8 @@ int op_add(int a, int b)
 /**
  * op_sub - subtracts two numbers
  *
- * @a: first number
- * @b: second number
+ * @a: first number to be subtracted
+ * @b: second number to be subtacted
  *
  * Return: the result of the operation
  */
@@ -29,8 +29,8 @@ int op_sub(int a, int b)
 /**
  * op_mul - multiplies two numbers
  *
- * @a: first number
- * @b: second number
+ * @a: first number to be multiplied
+ * @b: second number to be multiplied
  *
  * Return: the result of the operation
  */
@@ -42,8 +42,8 @@ int op_mul(int a, int b)
 /**
  * op_div - divides two numbers
  *
- * @a: first number
- * @b: second number
+ * @a: first number to be divided
+ * @b: second number to be divided
  *
  * Return: the result of the operation
  */
@@ -59,8 +59,8 @@ return (a / b);
 
 /**
  * op_mod - gets the remainder of the division between two numbers
- * @a: first number
- * @b: second number
+ * @a: first number to be divided
+ * @b: second number to be divided
  *
  * Return: the result of the operation
  */
